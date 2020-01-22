ed5f2f53363f50f773c0673fd88aed42fffcd947
VBolloj1 <Vamsikrishna.Bolloju1@T-Mobile.com>
2020-01-22 20:12:12 +0530

    development

--- src/app.js
+++ src/app.js
@@ -5,6 +5,20 @@ const port = process.env.PORT || 3000;
 
 const userSockets = {};
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/', (req, res)=> {
+    return res.end('Track your friend socket api is working...')
+});
+
+app.get('/sockets', (req, res) => {
+    return res.json(Object.keys(userSockets));
+    Object.keys(userSockets).filter( user => userSockets ) 
+});
 
 io.on('connection', socket => {
     console.log(' connection ', socket.id);
@@ -21,13 +35,16 @@ io.on('connection', socket => {
 
     socket.on('fetchLocation', (friend_name) => {
         console.log(`${current_user_name} wants to locate ${friend_name}`);
-        userSockets[friend_name].once('responseLocation', location => {
-            socket.emit('responseLocation', {  location, friend_name : current_user_name } );
-        });
-        userSockets[friend_name].emit('requestLocation', current_user_name);           
+        if(userSockets[friend_name]){
+            userSockets[friend_name].once('responseLocation', location => {
+                socket.emit('responseLocation', {  location, friend_name } );
+            });
+            userSockets[friend_name].emit('requestLocation', current_user_name);
+        }
     })
 
     socket.on('disconnect', (reason) => {
+        delete userSockets[socket.id];
         console.log( `Socket ${socket.id} is closed due to ${reason}`);
     });
 
